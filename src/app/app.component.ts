85ae01ca6c2c63d3ce7c402fcbb8d06e12e9dab3
Zurc <cruzjurado@gmail.com>
2017-05-12 10:35:55 +0100

    added field isFullTime to your model and binding to checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,18 @@ export class AppComponent {
   title = 'Angular 2 forms';
 
   languages = ['English', 'Spanish', 'Other'];
-  model = new Employee('coco', 'Washington');
+  model = new Employee('coco', 'Washington', true);
 
-  firstNameToUppercase(value: string) {
-    // check that there is a value
-    if(value.length > 0)
-      // convert the first character to uppercase and live the rest as it is
-      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
-    else
-      // if length is zero still assign an empty string value
-      this.model.firstName = value;
-  }
+  
+
+  // firstNameToUppercase(value: string) {
+  //   // check that there is a value
+  //   if(value.length > 0)
+  //     // convert the first character to uppercase and live the rest as it is
+  //     this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+  //   else
+  //     // if length is zero still assign an empty string value
+  //     this.model.firstName = value;
+  // }
 
 }
