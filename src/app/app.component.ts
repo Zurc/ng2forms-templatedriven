a837233e5552e97a0a924cd177eedf0f835b8da8
Zurc <cruzjurado@gmail.com>
2017-05-12 10:28:05 +0100

    calling a function. do NOT use the banana in a box syntax for this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,16 @@ export class AppComponent {
   title = 'Angular 2 forms';
 
   languages = ['English', 'Spanish', 'Other'];
-  model = new Employee('Coco', 'Washington');
+  model = new Employee('coco', 'Washington');
+
+  firstNameToUppercase(value: string) {
+    // check that there is a value
+    if(value.length > 0)
+      // convert the first character to uppercase and live the rest as it is
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    else
+      // if length is zero still assign an empty string value
+      this.model.firstName = value;
+  }
 
 }
